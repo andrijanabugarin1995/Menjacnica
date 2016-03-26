a5583743266e2c13ee0a3035366310ca59941e98
andrijanabugarin1995 <andrijana.bugarin@gmail.com>
2016-03-26 18:04:07 +0100

    Ispisane metode za klasu Menjacnica koja implementira interfejs

--- Menjacnica/src/Menjacnica.java
+++ Menjacnica/src/Menjacnica.java
@@ -4,23 +4,31 @@ import interfejs.MenjacnicaInterfejs;
 import menjacnica.Valuta;
 
 public class Menjacnica implements MenjacnicaInterfejs {
-
+	private LinkedList<Valuta> kursevi = new LinkedList<Valuta>();
+	
 	@Override
 	public void dodajKurs(Valuta kursZaDan) {
-		// TODO Auto-generated method stub
+		kursevi.add(kursZaDan);
 
 	}
 
 	@Override
 	public void obrisiKurs(Valuta kursZaDan) {
-		// TODO Auto-generated method stub
+		kursevi.remove(kursZaDan);
 
 	}
 
 	@Override
 	public LinkedList<Valuta> pronadjiVratiKurs(String naziv, String skraceniNaziv, String kurs) {
-		// TODO Auto-generated method stub
-		return null;
+		LinkedList<Valuta> rezultat = new LinkedList<Valuta>();
+		for (int i = 0; i < kursevi.size(); i++) {
+			if(kursevi.get(i).getKurs().contains(kurs)){
+				rezultat.add(kursevi.get(i));
+			}
+		}
+		return rezultat;
 	}
+	
+	
 
 }
