c64225f0d3507823913e05d756b3a5d50cd342ce
andrijanabugarin1995 <andrijana.bugarin@gmail.com>
2016-03-26 17:01:02 +0100

    Napravljena klasa Valuta u paketu Menjacnica, uneti atributi, get i set metode

---
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -0,0 +1,32 @@
+package menjacnica;
+
+public class Valuta {
+	String naziv;
+	String skraceniNaziv;
+	String kurs;
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+
+	public String getKurs() {
+		return kurs;
+	}
+
+	public void setKurs(String kurs) {
+		this.kurs = kurs;
+	}
+
+}
