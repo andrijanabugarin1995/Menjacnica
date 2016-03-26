0b22fe9366977a8f394cc27efe6339b965942f1d
andrijanabugarin1995 <andrijana.bugarin@gmail.com>
2016-03-26 17:52:13 +0100

    Uvedena klasa Menjacnica koja implementria MenjacnicaInterfejs

---
+++ Menjacnica/src/Menjacnica.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+import interfejs.MenjacnicaInterfejs;
+import menjacnica.Valuta;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(Valuta kursZaDan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(Valuta kursZaDan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public LinkedList<Valuta> pronadjiVratiKurs(String naziv, String skraceniNaziv, String kurs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
